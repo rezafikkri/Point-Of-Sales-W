43c059c430cb7bfbd297c16ad79e83ab1984cc7f
fikkrireza <fikkri.reza@gmail.com>
2021-10-15 11:08:33 +0700

    feat: add product categories table

---
+++ app/Database/Migrations/2021-10-15-035322_AddProductCategories.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddProductCategories extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField('product_category_id UUID PRIMARY KEY');
+        $this->forge->addField([
+            'product_category_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP'
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP'
+            ]
+        ]);
+        $this->forge->createTable('product_categories');
+    }
+
+    public function down()
+    {
+		$this->forge->dropTable('product_categories');
+    }
+}
